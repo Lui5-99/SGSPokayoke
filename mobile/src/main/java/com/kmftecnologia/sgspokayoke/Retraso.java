1d18574d7a6d14e9aff5acbf1eea80a72cadefd4
Lui5-99 <luis_bravo_99@hotmail.com>
2022-04-26 19:21:17 -0500

    Instalador SQLite Browser

---
+++ mobile/src/main/java/com/kmftecnologia/sgspokayoke/Retraso.java
@@ -0,0 +1,13 @@
+package com.kmftecnologia.sgspokayoke;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Retraso extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_retraso);
+    }
+}
